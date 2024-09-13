b42819b9aea330ba6c2bf77c467eacc2f628fab4
ShVira <viraskurenko@gmail.com>
2024-09-13 10:52:47 +0300

    Add project files.

---
+++ Статические члены класса/Time.cpp
@@ -0,0 +1,45 @@
+﻿#include "Time.h"
+
+#include <ctime>
+#include <iostream>
+
+using namespace std;
+
+unsigned int Time::s_createdObjects = 0U;
+unsigned int Time::s_liveObjects = 0U;
+
+Time::Time(const unsigned int hour, const unsigned int minute, const unsigned int second) :
+    m_hour(hour),
+    m_minute(minute),
+    m_second(second)
+{
+    ++s_createdObjects;
+    ++s_liveObjects;
+}
+
+Time::~Time()
+{
+    --s_liveObjects;
+}
+
+void Time::InfoAboutObjects()
+{
+    cout << "Created objects: " << s_createdObjects << endl;
+    cout << "Live objects: " << s_liveObjects << endl;
+}
+
+void Time::Show()
+{
+    cout << m_hour << ":" << m_minute << ":" << m_second << endl;
+}
+
+void Time::ShowCurrentTime()
+{
+    char date[50] = {};
+
+    time_t unixTime = time(nullptr);
+
+    ctime_s(date, sizeof(date) / sizeof(date[0]), &unixTime);
+
+    cout << date << endl;
+}
---
+++ Статические члены класса/Time.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+class Time
+{
+private:
+
+    unsigned int m_hour;
+    unsigned int m_minute;
+    unsigned int m_second;
+
+public:
+
+    static unsigned int s_createdObjects;
+    static unsigned int s_liveObjects;
+
+    Time(const unsigned int hour, const unsigned int minute, const unsigned int second);
+
+    ~Time();
+
+    static void InfoAboutObjects();
+    static void ShowCurrentTime();
+
+    void Show();
+};
